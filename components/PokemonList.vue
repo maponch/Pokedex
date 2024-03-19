6cda68e7e560d01e22ffb3ac4a6eecbd73813419
maponch <ma.ponchelet@gmail.com>
2024-03-19 09:40:56 +0100

    ajout du bouton précédent

--- components/PokemonList.vue
+++ components/PokemonList.vue
@@ -3,6 +3,7 @@
   <span v-if="$fetchState.pending">Loading...</span>
   <div v-else>
     <pre>{{  data }}</pre>
+    <button type="button" @click="goPreviousPage">précédent</button>
     <button type="button" @click="goNextPage">suivant</button>
   </div>
 </div>
@@ -13,21 +14,33 @@ export default {
     return {
       pokedexPath: 'https://pokeapi.co/api/v2/pokemon',
       pageNextPath: null,
+      pagePreviousPath: null,
       data: '',
     }
   },
   async fetch() {
-    await this.getPokemons()
+    await this.getNextPokemons()
+    await this.getPreviousPokemons()
   },
   methods: {
-    async getPokemons() {
+    async getNextPokemons() {
       const {results, next} = await this.$axios.$get(this.pokedexPath)
       this.data = results
       this.pageNextPath = next
     },
-
+    async getPreviousPokemons(){
+      const {results, previous} = await this.$axios.$get(this.pokedexPath)
+      this.data = results
+      console.log(previous)
+      this.pagePreviousPath = previous
+    },
     goNextPage() {
       this.pokedexPath = this.pageNextPath
+      this.$fetch()
+    },
+
+    goPreviousPage() {
+      this.pokedexPath = this.pagePreviousPath
       this.$fetch()
     }
   }
