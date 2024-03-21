8c3b65a2460008f9a365921a14144aa9c05192b1
maponch <ma.ponchelet@gmail.com>
2024-03-21 16:11:03 +0100

    recuperation du type par appel api

--- components/PokemonCard.vue
+++ components/PokemonCard.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <h1>Carte Pokémon</h1>
-    <pre>{{ pokemon.name }}</pre> <!-- Affichez les données pokemon -->
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>{{ pokemon.name }}</span>
+          <img :src="pokemon.sprites.front_default"
+            :alt="pokemon.name" />
+      </div>
+      <div class="text item">
+        <li v-for=" t in pokemon.types"> type : {{ t.type.name }}</li>
+        <li v-for=" m in pokemon.moves"> attaque : {{ m.move.name }}</li>
+      </div>
+    </el-card>
+    <!-- Affichez les données pokemon -->
+
   </div>
 </template>
 
@@ -9,9 +20,26 @@
 export default {
   props: {
     pokemon: {
-      type: [],
       required: true
     }
+  },
+  data(){
+    return {
+      avantageFaiblessePath: 'https://pokeapi.co/api/v2/type/'
+    }
+  },
+  async fetch(){
+    await this.getTypePokemon()
+  },
+  methods: {
+    display(){
+      console.log('tdc : ',this.pokemon)
+    },
+    async getTypePokemon() {
+      this.type = await this.$axios.$get(this.avantageFaiblessePath)
+      console.log('grrrrrr',this.type)
+      console.log('tdc : ', this.pokemon)
+  },
   }
 }
 </script>
